c8d83b07fee143a096ab5a762d349e90408b7ef8
Danny Fuhriman <danny.fuhriman@Okta.com>
2021-11-05 15:15:41 -0700

    update variable types & defaults

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,26 +1,38 @@
 # TF Variables
 variable "org_name" {
+  type = string
 }
 
 variable "api_token" {
+  type = string
 }
 
 variable "base_url" {
+  type = string
   default = "okta.com" #"oktapreview.com" if using non-prod.
 }
 
 variable "app_url" {
+  type = string
+  default= "http://localhost:8080"
+}
+variable "byob_app_url" {
+  type = string
+  default= "http://localhost:8081"
 }
 
 variable "superuser_login" {
+  type = string
 
 }
 
 variable "environment" {
+  type = string
   default = "dev"
 }
 
 variable "aws_region" {
+  type = string
   default = "us-east-1"
 }
 
@@ -28,5 +40,6 @@ variable "aws_profile" {
 }
 
 variable "aws_ssm_prefix" {
+  type = string
   default = "dac"
 }
