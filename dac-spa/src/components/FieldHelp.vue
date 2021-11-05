de40c6a4804f4ed6176791eeaea460a6cca16cb9
Danny Fuhriman <danny.fuhriman@Okta.com>
2021-11-05 15:27:00 -0700

    formatting => add prettier/es-lint

--- dac-spa/src/components/FieldHelp.vue
+++ dac-spa/src/components/FieldHelp.vue
@@ -1,25 +1,23 @@
+<!-- @format -->
+
 <template>
-    <v-tooltip bottom>
-        <template v-slot:activator="{ on }">
-            <v-icon v-on="on" size="16" class="mt-1">
-                mdi-help-circle-outline
-            </v-icon>
-        </template>
-        <v-card 
-            flat color="transparent"
-            max-width="330"
-            class="mx-auto"
-            >
-            <div class="white--text caption">{{ text }}</div>
-        </v-card>
-    </v-tooltip>
+	<v-tooltip bottom>
+		<template v-slot:activator="{ on }">
+			<v-icon v-on="on" size="16" class="mt-1">
+				mdi-help-circle-outline
+			</v-icon>
+		</template>
+		<v-card flat color="transparent" max-width="330" class="mx-auto">
+			<div class="white--text caption">{{ text }}</div>
+		</v-card>
+	</v-tooltip>
 </template>
 
 <script>
 export default {
-    name: 'field-help',
-    props: {
-        text: String
-    }
-}
+	name: 'field-help',
+	props: {
+		text: String,
+	},
+};
 </script>
--- dac-spa/src/components/Tokens.vue
+++ dac-spa/src/components/Tokens.vue
@@ -1,37 +1,39 @@
+<!-- @format -->
+
 <template>
-  <div>
-    <v-container fluid grid-list-md>
-      <v-layout row wrap>
-        <OktaToken type="accessToken"></OktaToken>
-        <OktaToken type="idToken"></OktaToken>
-        <OktaToken type="o4oToken"></OktaToken>
-      </v-layout>
-    </v-container>
-  </div>
+	<div>
+		<v-container fluid grid-list-md>
+			<v-layout row wrap>
+				<OktaToken type="accessToken"></OktaToken>
+				<OktaToken type="idToken"></OktaToken>
+				<OktaToken type="o4oToken"></OktaToken>
+			</v-layout>
+		</v-container>
+	</div>
 </template>
 
 <script>
-import OktaToken from "@/components/Token";
+import OktaToken from '@/components/Token';
 
 export default {
-  name: "tokens",
-  data() {
-    return {};
-  },
-  components: {
-    OktaToken: OktaToken
-  }
+	name: 'tokens',
+	data() {
+		return {};
+	},
+	components: {
+		OktaToken: OktaToken,
+	},
 };
 </script>
 
 <style scoped>
 .flip-container {
-  perspective: 1000px;
+	perspective: 1000px;
 }
 
 .flip-container,
 .front,
 .back {
-  height: 100%;
+	height: 100%;
 }
 </style>
