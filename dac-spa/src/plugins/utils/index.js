14bb22c16e91c6d69df9e33545e0411242d8e462
Danny Fuhriman <danny.fuhriman@Okta.com>
2021-11-05 15:30:09 -0700

    remove unused variables

--- dac-spa/src/plugins/utils/index.js
+++ dac-spa/src/plugins/utils/index.js
@@ -1,32 +1,30 @@
-import moment from "moment";
+/** @format */
+
+import moment from 'moment';
 
 class Util {
-    constructor() {
+	constructor() {}
+	urlParams(query) {
+		let urlParams = {};
+		const search = /([^&=]+)=?([^&]*)/g;
 
-    }
-    urlParams(query) {
-        let urlParams = {};
-        const search = /([^&=]+)=?([^&]*)/g;
-    
-        let match;
-        while ((match = search.exec(query)) != null) {
-            urlParams[match[1]] = match[2];
-        }
+		let match;
+		while ((match = search.exec(query)) != null) {
+			urlParams[match[1]] = match[2];
+		}
 
-        return urlParams;    
-    }
-    offsetDate(date, format) {
-        if (!date || date.length <= 0) return '';
-        
-        const pattern = format ? format : 'MM/DD/YYYY HH:mm:ss'
-        return moment(moment.utc(date).toDate())
-            .local()
-            .format(pattern);
-    }
+		return urlParams;
+	}
+	offsetDate(date, format) {
+		if (!date || date.length <= 0) return '';
+
+		const pattern = format ? format : 'MM/DD/YYYY HH:mm:ss';
+		return moment(moment.utc(date).toDate()).local().format(pattern);
+	}
 }
 
-function install (Vue, options) {
-  Vue.prototype.$utils = new Util()
+function install(Vue) {
+	Vue.prototype.$utils = new Util();
 }
 
-export default { install }
+export default { install };
