f90af47828292173d0815035b376dbfcb8d1b701
Danny Fuhriman <danny.fuhriman@Okta.com>
2021-11-05 19:57:55 -0700

    Update vue.config.js

--- dac-spa/vue.config.js
+++ dac-spa/vue.config.js
@@ -2,4 +2,14 @@
 
 module.exports = {
 	transpileDependencies: ['vuetify', 'vuex-persist'],
+	css: {
+		loaderOptions: {
+			sass: {
+				sassOptions: {
+					quiet: true,
+					quietDeps: true,
+				},
+			},
+		},
+	},
 };
